ffe05a794c994afbf36ae3e60ae4b065e60d08c1
Chester <chesterdeveloper2000@gmail.com>
2020-07-26 12:46:33 +0800

    extracted countdownTittle and countdownDate

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ const countdownForm = document.getElementById("countdownForm")
 const dateEl = document.getElementById("date-picker")
 
 
+let countdownTitle = ""
+let countdownDate = ""
+
 // set date input min with today's date
 const today = new Date().toISOString().split("T")[0]
 
 dateEl.setAttribute("min", today)
 
 
+// take values forom form input
+updateCountdown = (e) => {
+    e.preventDefault()
+    countdownTitle = e.srcElement[0].value
+    countdownDate = e.srcElement[1].value
+    console.log(countdownTitle)
+    console.log(countdownDate)
+}
 
+// event listener
+countdownForm.addEventListener("submit", updateCountdown)
