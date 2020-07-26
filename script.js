5cb85331abbdc00cddf0962cfcbfc37d9ca8e02a
Chester <chesterdeveloper2000@gmail.com>
2020-07-26 12:59:19 +0800

    computed the current time vs. the set date (value of it still has to be converted)

--- script.js
+++ script.js
@@ -2,14 +2,29 @@ const inputContainer = document.getElementById("input-container")
 const countdownForm = document.getElementById("countdownForm")
 const dateEl = document.getElementById("date-picker")
 
+const countdownEl = document.getElementById("countdown")
+const countdownElTitle = document.getElementById("countdown-title")
+const countdownButton = document.getElementById("countdown-button")
+const timeElements = document.querySelectorAll("span")
+
 
 let countdownTitle = ""
 let countdownDate = ""
+let countdownValue = Date
+
 
 // set date input min with today's date
 const today = new Date().toISOString().split("T")[0]
-
 dateEl.setAttribute("min", today)
+
+
+// populate countdown and complete ui
+updateDom = () => {
+    const now = new Date().getTime()
+    const distance = countdownValue - now
+    console.log("distance", distance)
+
+}
 
 
 // take values forom form input
@@ -19,6 +34,10 @@ updateCountdown = (e) => {
     countdownDate = e.srcElement[1].value
     console.log(countdownTitle)
     console.log(countdownDate)
+    // get the number version of current date
+    countdownValue = new Date(countdownDate).getTime()
+    console.log("countdownValue" ,countdownValue)
+    updateDom()
 }
 
 // event listener
