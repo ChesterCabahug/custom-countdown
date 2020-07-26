113e2aa732d56cc38f7d92756dd00218a147f89e
Chester <chesterdeveloper2000@gmail.com>
2020-07-26 13:37:58 +0800

    it now actually counts down

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const timeElements = document.querySelectorAll("span")
 let countdownTitle = ""
 let countdownDate = ""
 let countdownValue = Date
+let countdownActive
 
 const second = 1000
 const minute = second * 60
@@ -24,31 +25,31 @@ dateEl.setAttribute("min", today)
 
 // populate countdown and complete ui
 updateDom = () => {
-    const now = new Date().getTime()
-    const distance = countdownValue - now
-    console.log("distance", distance)
+    countdownActive = setInterval(() => {
+        const now = new Date().getTime()
+        const distance = countdownValue - now
+        console.log("distance", distance)
 
-    const days = Math.floor(distance / day)
-    const hours = Math.floor((distance % day) / hour)
-    const minutes = Math.floor((distance % hour) / minute)
-    const seconds = Math.floor((distance % minute) / second)
+        const days = Math.floor(distance / day)
+        const hours = Math.floor((distance % day) / hour)
+        const minutes = Math.floor((distance % hour) / minute)
+        const seconds = Math.floor((distance % minute) / second)
 
-    console.log(days, hours, minutes, seconds)
+        console.log(days, hours, minutes, seconds)
 
-    // populate our countdown
-    countdownElTitle.textContent = `${countdownTitle}`
-    timeElements[0].textContent = `${days}`
-    timeElements[1].textContent = `${hours}`
-    timeElements[2].textContent = `${minutes}`
-    timeElements[3].textContent = `${seconds}`
+        // populate our countdown
+        countdownElTitle.textContent = `${countdownTitle}`
+        timeElements[0].textContent = `${days}`
+        timeElements[1].textContent = `${hours}`
+        timeElements[2].textContent = `${minutes}`
+        timeElements[3].textContent = `${seconds}`
 
-    // hide input
-    inputContainer.hidden = true
+        // hide input
+        inputContainer.hidden = true
 
-    // show countdown
-    countdownEl.hidden = false
-
-
+        // show countdown
+        countdownEl.hidden = false
+    }, second)
 }
 
 
